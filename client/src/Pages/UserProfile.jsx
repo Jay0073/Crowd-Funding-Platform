9f4044e6fbee9d11d22e31de12bb2be736786db4
Jayendra <voutlajay8765@gmail.com>
2025-03-13 23:36:53 +0530

    remove unused pages: Admindashboard, ContactUs, PageNotFound, and Userdashboard; update UserProfile styling

--- client/src/Pages/UserProfile.jsx
+++ client/src/Pages/UserProfile.jsx
@@ -237,7 +237,7 @@ const UserProfile = () => {
   }
 
   return (
-    <div className="min-h-screen bg-gray-50 py-12">
+    <div className="min-h-screen bg-gray-50 pb-12 pt-26">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <motion.div
           variants={containerVariants}
